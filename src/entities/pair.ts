1f948c197eda5d9362aa8bbef18ab59533e81c36
christian <51129408+chdomenech@users.noreply.github.com>
2022-02-20 08:24:05 -0500

    Cambio de nombre de version y par

--- src/entities/pair.ts
+++ src/entities/pair.ts
@@ -42,8 +42,8 @@ export class Pair {
       tokenAmounts[0].token.chainId,
       Pair.getAddress(tokenAmounts[0].token, tokenAmounts[1].token),
       18,
-      'Cake-LP',
-      'Pancake LPs'
+      'Tova-LP',
+      'TovaSwap LPs'
     )
     this.tokenAmounts = tokenAmounts as [TokenAmount, TokenAmount]
   }
